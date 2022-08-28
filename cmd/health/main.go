3545e576dd1ea671a39cb90fbfba673b3d31ec1d
Diogo Pais <dpais11@gmail.com>
2022-08-29 00:31:56 +0100

    chore: remove custom JSON parsing

--- cmd/health/main.go
+++ cmd/health/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"time"
 
@@ -10,18 +9,10 @@ import (
 )
 
 func main() {
-	config := getConfig()
-
-	health.Start(health.Config{
-		Port:           config.getString("port"),
-		Path:           config.getString("path"),
-		RequestTimeout: config.getDuration("requestTimeout"),
-		CheckInterval:  config.getDuration("checkInterval"),
-		Services:       config.getServices("services"),
-	})
+	health.Start(getConfig())
 }
 
-func getConfig() (config jsonObject) {
+func getConfig() (config health.Config) {
 	configPath := getConfigPath()
 
 	data, err := os.ReadFile(configPath)
@@ -29,6 +20,9 @@ func getConfig() (config jsonObject) {
 
 	err = json.Unmarshal(data, &config)
 	check(err)
+
+	config.RequestTimeout *= time.Millisecond
+	config.CheckInterval *= time.Millisecond
 
 	return
 }
@@ -55,59 +49,4 @@ func check(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-type jsonObject map[string]any
-
-func (object jsonObject) getArray(key string) []any {
-	return getObjectValue[[]any](object, key)
-}
-
-func (object jsonObject) getDuration(key string) time.Duration {
-	return time.Duration(object.getNumber(key)) * time.Millisecond
-}
-
-func (object jsonObject) getNumber(key string) float64 {
-	return getObjectValue[float64](object, key)
-}
-
-func (object jsonObject) getString(key string) string {
-	return getObjectValue[string](object, key)
-}
-
-func (object jsonObject) getServices(key string) (services []health.ServiceConfig) {
-	rawServices := object.getArray(key)
-
-	services = make([]health.ServiceConfig, len(rawServices))
-
-	for i, rawService := range rawServices {
-		o := jsonObject(castValue[map[string]any](rawService))
-
-		services[i] = health.ServiceConfig{
-			Name: o.getString("name"),
-			Url:  o.getString("url"),
-		}
-	}
-
-	return
-}
-
-func getObjectValue[T any](o jsonObject, key string) (value T) {
-	defer func() {
-		if r := recover(); r != nil {
-			panic(fmt.Sprintf("Value of key %#v in JSON object has value %#v which cannot be cast to type %T.", key, o[key], value))
-		}
-	}()
-
-	return castValue[T](o[key])
-}
-
-func castValue[T any](value any) T {
-	v, ok := value.(T)
-
-	if !ok {
-		panic(fmt.Sprintf("Value %#v cannot be cast to type %T.", value, v))
-	}
-
-	return v
 }
